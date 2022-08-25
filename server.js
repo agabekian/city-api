64914f70fae8657bd6260462e6c394b7db422048
Agabekian <armasconi@gmail.com>
2022-08-24 18:31:41 -0700

    ok

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+const axios = require("axios")
 
 console.log('Am running or what?! Upgraded to 22');
 
@@ -32,28 +33,75 @@ app.get('/', (request, response) => {
 });
 
 
-app.get('/weather', (request, response, next) => {
-  try {
-    let name = request.query.name;
-    // console.log(typeof name)
-    console.log(data[0].city_name);
-    let dataToGroom = data.find(obj => obj.city_name === name);
-    console.log(dataToGroom);
-    let dataToSend = dataToGroom.data.map(d => {return new Forecast(d)});
-    response.status(200).send(dataToSend);
-  } catch (error) {
-    // if I have an error, this will create a new instance of the Error Object that lives in Express.
-    next(error);
-  }
-});
+// app.get('/weather', (request, response, next) => {
+//   try {
+//     let name = request.query.name;
+//     // console.log(typeof name)
+//     console.log(data[0].city_name);
+//     let dataToGroom = data.find(obj => obj.city_name === name);
+//     console.log(dataToGroom);
+//     let dataToSend = dataToGroom.data.map(d => {return new Forecast(d)});
+//     response.status(200).send(dataToSend);
+//   } catch (error) {
+//     // if I have an error, this will create a new instance of the Error Object that lives in Express.
+//     next(error);
+//   }
+// });
 
 class Forecast {
   constructor(resObj) {
     this.date = resObj.datetime;
+    this.high_temp = resObj.high_temp;
     this.description = resObj.weather.description;
   }
 }
 
+class Movie {
+  constructor(resObj) {
+    this.title = resObj.original_title;
+  }
+}
+
+// Endpoints
+app.get('/weather', getWeather);
+app.get('/movies', getMovies);
+
+async function getWeather(request, response) {
+  // note the .searchQuery
+  const { lat, lon } = request.query;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&days=5`;
+  try {
+    // axios get request to our api
+    const weatherResponses = await axios.get(url);
+    // console.log(">>>>>>>>>>>>>>>>>"+weatherResponses.data.weather);
+    // groomed data
+    const forecastArray = weatherResponses.data.data.map(f => new Forecast(f));
+    console.log("FINAL PRODUCT", forecastArray);
+    response.status(200).send(forecastArray);
+  } catch (err) {
+    console.log('error message is: ', err);
+    response.status(500).send(`server error`);
+  }
+}
+
+
+async function getMovies(request, response) {
+  console.log("Hurray")
+  const { city } = request.query;
+  console.log(city)
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${city}&page=1&include_adult=false`;
+
+  try {
+    const moviesBack = await axios.get(url);
+    console.log(moviesBack.data.results)
+    const movieArr = moviesBack.data.results.map(m=>new Movie(m));
+    console.log(movieArr);
+    response.status(200).send(movieArr);
+  } catch (err) {
+    console.log('error message is: ', err);
+    response.status(500).send(`server error`);
+  }
+}
 
 
 // ERRORS
